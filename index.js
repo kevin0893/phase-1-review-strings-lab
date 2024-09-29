afca3e205cbbc065a9d5064eca38fcb62f7fe704
+ <cheruiyotke95@gmail.com>
2024-09-29 09:55:09 +0300

    phase 1 review strings lab.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = "Kevin Cheruiyot";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+       
